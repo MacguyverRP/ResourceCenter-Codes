a4553c8c718010bee63cba601dffed84a0579431
Sokiri <Sokiri@W06-Cardinal>
2021-07-22 02:09:49 +0800

    added comments for retrieve all chromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -146,8 +146,7 @@ public class ResourceCentre {
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
 		// write your code here
-		for (int i = 0; i < chromebookList.size(); i++) {
-
+		for (int i = 0; i < chromebookList.size(); i++) { 
 			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", chromebookList.get(i).getAssetTag(),
 					chromebookList.get(i).getDescription(), 
 					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
